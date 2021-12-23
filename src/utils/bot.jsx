6e65859c9739470629bd63769f97e6a5cf46f705
Mukul <mukulkumar627@gmail.com>
2021-12-23 10:34:27 +0530

    Add minimax bot

---
+++ src/utils/bot.jsx
@@ -0,0 +1,109 @@
+import { is_moves_left, evaluate } from './gameUtils';
+
+function minimax(board, depth, isMax, player, ai) {
+
+    const score = evaluate(board);
+    if(depth == 0) return score;
+    // If Maximizer or minimizer has won the game return his/her
+    // evaluated score
+    if (score === 10 || score === -10)
+        return score;
+
+    // If there are no more moves and no winner then
+    // it is a tie
+    if (is_moves_left(board) === false)
+        return 0;
+
+    // If this maximizer's move
+    if (isMax) {
+        let best = -1000;
+
+        // Traverse all cells
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                // Check if cell is empty
+                if (board[i][j] === 0) {
+                    // Make the move
+                    board[i][j] = player;
+
+                    // Call minimax recursively and choose
+                    // the maximum value
+                    best = Math.max(best,
+                        minimax(board, depth - 1, !isMax, player, ai));
+
+                    // Undo the move
+                    board[i][j] = 0;
+                }
+            }
+        }
+        return best;
+    }
+
+    // If this minimizer's move
+    else {
+        let best = 1000;
+
+        // Traverse all cells
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                // Check if cell is empty
+                if (board[i][j] === 0) {
+                    // Make the move
+                    board[i][j] = ai;
+
+                    // Call minimax recursively and choose
+                    // the minimum value
+                    best = Math.min(best,
+                        minimax(board, depth - 1, !isMax, player, ai));
+
+                    // Undo the move
+                    board[i][j] = 0;
+                }
+            }
+        }
+        return best;
+    }
+}
+
+function findBestMove(board, player, ai) {
+    let bestVal = 100000;
+    let bestRow = -1;
+    let bestCol = -1;
+
+    // Traverse all cells, evaluate minimax function for
+    // all empty cells. And return the cell with optimal
+    // value.
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (board[i][j] === 0) {
+                // Make the move
+                board[i][j] = ai;
+
+                // compute evaluation function for this
+                // move.
+                let moveVal = minimax(board, 5, true, player, ai); // 10 moves ahead
+
+                board[i][j] = 0;
+
+                if (moveVal < bestVal) {
+                    bestRow = i;
+                    bestCol = j;
+                    bestVal = moveVal;
+                }
+            }
+        }
+    }
+
+    return [bestRow, bestCol, bestVal];
+}
+
+// const board = [
+//     [2,2,2,0],
+//     [1,1,1,0],
+//     [0,1,0,0],
+//     [0,0,0,0]
+// ];
+
+// console.log(findBestMove(board, 1, 2));
+
+export { findBestMove };
---
+++ src/utils/gameUtils.jsx
@@ -0,0 +1,60 @@
+function evaluate(board) {
+    /*
+        This function implements the logic to check the end of the game 
+        either through a win or a draw. Player 1 wins +10, Player 2 wins - 10
+        Draw is a 0;
+    */
+    for (let row = 0; row < 4; row++) {
+        if (board[row][0] === board[row][1] && board[row][1] === board[row][2] && board[row][2] === board[row][3]) {
+            if (board[row][0] === 1) return 10;
+            else if (board[row][0] === 2) return -10;
+        }
+    }
+
+    for (let col = 0; col < 4; col++) {
+        if (board[0][col] === board[1][col] && board[1][col] === board[2][col] && board[2][col] === board[3][col]) {
+            if (board[0][col] === 1) return 10;
+            if (board[0][col] === 2) return -10;
+
+        }
+    }
+
+    // check main diagonal
+    const top = board[0][0];
+    let diag_f = 1;
+    for (let i = 1; i < 4; i++) {
+        if (board[i][i] !== top) { diag_f = 0; break; }
+    }
+    if (diag_f) {
+        if (top === 1) return 10;
+        if (top === 2) return -10;
+    }
+
+    // the other diagonal
+    const right_top = board[0][3];
+    let diag_f2 = 1;
+    for (let i = 1, j = 2; i < 4, j >= 0; i++, j--) {
+        if (board[i][j] !== right_top) {
+            diag_f2 = 0;
+            break;
+        }
+    }
+
+    if (diag_f2) {
+        if (right_top === 1) return 10;
+        if (right_top === 2) return -10;
+    }
+
+    return 0;
+}
+
+function is_moves_left(board) {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (board[i][j] === 0) return true;
+        }
+    }
+    return false;
+}
+
+export { evaluate, is_moves_left };
