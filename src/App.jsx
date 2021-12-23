7ac623e31d1f98bc857ba4d59c4875aa9529370a
Mukul <mukulkumar627@gmail.com>
2021-12-23 11:01:02 +0530

    polish offline multiplayer and AI game

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Play from './Components/Play/Play';
 function App() {
   return <BrowserRouter>
     <Routes>
-      <Route path='/play' element={<GameBoard/>} />
+      <Route path='/play/human' element={<GameBoard player='human'/>} />
+      <Route path='/play/ai' element={<GameBoard player='ai'/>} />
       <Route path='/' element={<Play/>} />
     </Routes>
   </BrowserRouter>
--- src/Components/Play/Play.jsx
+++ src/Components/Play/Play.jsx
@@ -3,10 +3,17 @@ import playButton from '../../assets/play.png';
 import { useNavigate } from 'react-router';
 import './Play.css';
 
+import AI from '../../assets/AI.png';
+
 function Play(props) {
     const navigate = useNavigate();
-    return <div id='play-button' onClick={() => navigate('/play')}>
-        <img id='play-button' src={playButton} />
+    return <div className='play-wrapper'>
+        <div className='play-button' onClick={() => navigate('/play/human')}>
+            <img id='play-button' src={playButton} />
+        </div>
+        <div className='play-button' onClick={() => navigate('/play/ai')}>
+            <img id='play-button' src={AI} />
+        </div>       
     </div>
 }
 
